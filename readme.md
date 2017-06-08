cd84096f6738a747742ec110c2d1ae0916f8053a
Bruno Couty <brunocouty@gmail.com>
2017-06-07 22:22:02 -0300

    #0 - update on documentation

--- readme.md
+++ readme.md
@@ -23,13 +23,56 @@ Agora, adicione o *Service Provider* no *array* de *providers*, no arquivo "*con
 BrunoCouty\ValidatorDataBR\ValidatorDataBRServiceProvider::class,
 ```
 
-Para utilizar:
+Métodos disponíveis:
+
+* cpf;
+* cnpj;
+* cnh;
+* celular;
+* tituloEleitoral;
+* uf;
+* cc (Cartão de Crédito);
 
 ```php
 $validator = new \BrunoCouty\ValidatorDataBR\Services\ValidatorDataBR();
-$response = $validator->cpf('your-cpf');
-$response = $validator->cnpf('your-cnpj');
-$response = $validator->celular('your-celular');
+
+// Para todas as validações abaixo, o parâmetro é uma *string*.
+// As máscaras são tratadas, logo, você pode enviar como parâmetro "111.222.333-44"
+$validCPF = $validator->cpf('your-cpf');
+$validCNPJ = $validator->cnpf('your-cnpj');
+$validCNH = $validator->cnh('your-cnh');
+$validCelular = $validator->celular('your-celular');
+$validTituloEleitoral = $validator->tituloEleitoral('titulo-eleitor');
+$validUF = $validator->uf('unidade-federal');
 ```
 
-A resposta será um ***boolean*** (*true* ou *false*).
+Para validar cartões de crédito, utiliza-se dois parâmetros: *número do cartão* e bandeira. A bandeira pode ser passada da seguinte forma:
+
+* *ValidatorDataBR::CARD_AMEX*;
+* *ValidatorDataBR::CARD_DINERS*;
+* *ValidatorDataBR::CARD_DISCOVER*;
+* *ValidatorDataBR::CARD_MASTERCARD*;
+* *ValidatorDataBR::CARD_VISA*;
+* *ValidatorDataBR::CARD_ALL*;
+
+```php
+// $response = $validatorBR->cc('credit-card-number', 'static-card-flag');
+$response = $validatorBR->cc('5111111111111118', $validator::CARD_MASTERCARD);
+```
+
+A resposta sempre será um ***boolean*** (*true* ou *false*).
+
+## Agradecimento e Créditos
+
+Os métodos de cartão de crédito, título de eleitor e NIS foram retirados do repositório do [@paulofreitas](https://gist.github.com/paulofreitas/4704673). Dê uma passadinha lá, vale a pena!
+
+## Gostou deste conteúdo? Me pague um café!
+
+Yeah! Você gostou deste pacote? Me pague um café e me ajude a manter este pacote atualizado!
+
+Quando você me ajuda, você tem acesso a **posts exclusivos** com muitas coisas úteis sobre PHP, Laravel, AngularJS, VueJS, Ionic, e muito mais! Você aprenderá a criar seus próprios pacotes PHP (independentes de framework), resolver problemas em seu código fonte... Um excelente conteúdo, sempre atualizado!
+
+Você pode me ajudar com R$ 1 / mês e já terá acesso a meu conteúdo privado! 
+E mais, precisa de ajuda com seu projeto? Eu posso ajudar você! Acesse [https://apoia.se/brunocouty](https://www.apoia.se/brunocouty), posso te ajudar via e-mail ou skype!
+
+[https://apoia.se/brunocouty](https://www.apoia.se/brunocouty)
--- src/Services/ValidatorDataBR.php
+++ src/Services/ValidatorDataBR.php
@@ -4,6 +4,54 @@ namespace BrunoCouty\ValidatorDataBR\Services;
 
 class ValidatorDataBR
 {
+    /*
+     * Constantes das bandeiras do cartão de crédito
+     */
+    const CARD_AMEX = 1;
+    const CARD_DINERS = 2;
+    const CARD_DISCOVER = 4;
+    const CARD_MASTERCARD = 8;
+    const CARD_VISA = 16;
+    const CARD_ALL = 31;
+
+    /**
+     * Valida o número de um cartão de crédito de acordo com sua bandeira
+     * @param $cc
+     * @param $flag
+     * @return bool
+     */
+    public static function cc(string $cc, $flag = ValidatorDataBR::CARD_ALL)
+    {
+        // Canonicalize input
+        $cc = preg_replace('{\D}', '', $cc);
+        // Validate choosed flags
+        $er = array();
+        if ($flag & self::CARD_AMEX) {
+            $er[] = '^3[47].{13}$';
+        }
+        if ($flag & self::CARD_DINERS) {
+            $er[] = '^3(0[0-5].{11}|6.{12})$';
+        }
+        if ($flag & self::CARD_DISCOVER) {
+            $er[] = '^6(011.{12}|5.{14})$';
+        }
+        if ($flag & self::CARD_MASTERCARD) {
+            $er[] = '^5[1-5].{14}$';
+        }
+        if ($flag & self::CARD_VISA) {
+            $er[] = '^4.{15}$';
+        }
+        if (empty($er) || !preg_match('~' . implode('|', $er) . '~', $cc)) {
+            return false;
+        }
+        // Validate digits using a modulus 10 algorithm (aka Luhn)
+        for ($sum = 0, $idx = strlen($cc) - 1, $wt = 1; $idx >= 0;
+             $wt = ($wt % 2) + 1, --$idx) {
+            $sum += (($d = intval($cc[$idx]) * $wt) > 9) ? $d - 9 : $d;
+        }
+        return (($sum % 10) == 0);
+    }
+
     /**
      * Verifica se o CPF é válido
      * @param string $cpf
@@ -85,6 +133,35 @@ class ValidatorDataBR
     }
 
     /**
+     * Verifica se a CNH é válida
+     * @param string $cnh
+     * @return bool
+     */
+    public function cnh(string $cnh)
+    {
+        // Canonicalize input
+        $cnh = sprintf('%011s', preg_replace('{\D}', '', $cnh));
+        // Validate length and invalid numbers
+        if ((strlen($cnh) != 11) || (intval($cnh) == 0)) {
+            return false;
+        }
+        // Validate check digits using a modulus 11 algorithm
+        for ($c = $s1 = $s2 = 0, $p = 9; $c < 9; $c++, $p--) {
+            $s1 += intval($cnh[$c]) * $p;
+            $s2 += intval($cnh[$c]) * (10 - $p);
+        }
+        if ($cnh[9] != (($dv1 = $s1 % 11) > 9) ? 0 : $dv1) {
+            return false;
+        }
+        if ($cnh[10] != (((($dv2 = ($s2 % 11) - (($dv1 > 9) ? 2 : 0)) < 0)
+                ? $dv2 + 11 : $dv2) > 9) ? 0 : $dv2
+        ) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
      * Validar celular
      * @param string $phone
      * @return bool
@@ -121,6 +198,77 @@ class ValidatorDataBR
             return true;
         }
         return false;
+    }
+
+    /**
+     * Valida título eleitoral
+     * @param $te
+     * @return bool
+     */
+    public function tituloEleitoral($te)
+    {
+        $te = sprintf('%012s', preg_replace('{\D}', '', $te));
+        $uf = intval(substr($te, 8, 2));
+        if ((strlen($te) != 12)
+            || ($uf < 1)
+            || ($uf > 28)
+        ) {
+            return false;
+        }
+        foreach (array(7, 8 => 10) as $s => $t) {
+            for ($d = 0, $p = 2, $c = $t; $c >= $s; $c--, $p++) {
+                $d += $te[$c] * $p;
+            }
+            if ($te[($s) ? 11 : 10] != ((($d %= 11) < 2) ? (($uf < 3) ? 1 - $d
+                    : 0)
+                    : 11 - $d)
+            ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Valida número de inscrição social
+     * @param $nis
+     * @return bool
+     */
+    public function nis($nis)
+    {
+        $nis = sprintf('%011s', preg_replace('{\D}', '', $nis));
+        if ((strlen($nis) != 11)
+            || (intval($nis) == 0)
+        ) {
+            return false;
+        }
+        for ($d = 0, $p = 2, $c = 9; $c >= 0; $c--, ($p < 9) ? $p++ : $p = 2) {
+            $d += $nis[$c] * $p;
+        }
+        return ($nis[10] == (((10 * $d) % 11) % 10));
+    }
+
+    /**
+     * Valida o DDD de um telefone
+     * @param $ddd
+     * @return bool
+     */
+    public function ddd($ddd)
+    {
+        return preg_match('{^([14689][1-9]|2[14]|[23][278]|[357][13-5]|7[79])$}',
+                $ddd) != false;
+    }
+
+
+    /**
+     * Valida UF
+     * @param $uf
+     * @return bool
+     */
+    public static function uf($uf)
+    {
+        return preg_match('{^A[CLMP]|BA|CE|DF|ES|[GT]O|M[AGST]|P[ABEIR]|R[JNORS]'
+                . '|S[CEP]$}', $uf) != false;
     }
 
 
